6e22a7f391094e205b65b4786c8fff375f65488a
vsellathurai <sellathurai.vyach@yahoo.fr>
2015-02-03 01:23:18 +0100

    Menu bar fixed during scrolling  MyPortfolio 0.6

--- menu.php
+++ menu.php
@@ -1,22 +1,21 @@
-<?php include('fonctionsPHP/fonctionIfBonnePage.php')?>
+<?php include('fonctionsPHP/fonctionIfBonnePage.php') ?> 
 <!-- *********  Header  ********** -->
     
-    <div id="header">
+
+    
+    <div id="header" class="elemeInit">
         <div id="header_in">
-        
-        	<h1><a href="index.php"><b>Portfolio</b> Vyach Sellathurai</a></h1>
-        
-	        <div id="menu">
+        	<div id="logo"> <h1><a href="index.php"><b>Portfolio</b> Vyach Sellathurai</a></h1></div>
+        	 <div id="menu" class="menuBack">
 		         <ul>
-		            <li><a href="index.php" <?php ifBonnePage("index.php")?>>Accueil</a></li>
-		            <li><a href="competence.php" <?php ifBonnePage("competence.php")?>>Competences</a></li>
-		            <li><a href="projetsDisplay.php" <?php ifBonnePage("projetsDisplay.php"); ifBonnePage("getap.php"); ifBonnePage("weonestore.php"); ifBonnePage("zencours.php");?>>Projets</a></li>
+		            <li class="cheatBann"><a href="index.php" <?php ifBonnePage("index.php")?>>Accueil</a></li>
+		            <li class="cheatBann"><a href="competence.php" <?php ifBonnePage("competence.php")?>>Competences</a></li>
+		            <li><a href="projetsDisplay.php" <?php ifBonnePage("projetsDisplay.php"); ifBonnePage("getap.php"); ifBonnePage("weonestore.php"); ifBonnePage("zencours.php"); ifBonnePage("partitionneur.php");?>>Projets</a></li>
+		            <li><a href="#" <?php ifBonnePage("#")?>>Veille Technologique</a></li>
 		            <li><a href="cv.php" <?php ifBonnePage("cv.php")?>>CV</a></li>
 		            <li><a href="contact.php" <?php ifBonnePage("contact.php")?>>Contact</a></li>            
 		         </ul>
 	        </div>
-	        
-
 	    </div>
     </div>
 
@@ -26,5 +25,36 @@
 		$('#menu').slicknav();
 	});
 </script>
+<script>
+
+	/*$(window).scroll(function() {
+		if(!$('#menu').hasClass("menuFixed") && ($(window).scrollTop() > $('#menu').offset().top)) {
+			$('#menu').removeClass("menuBack");
+			$('#menu').addClass("menuFixed").data("top", $('#menu').offset().top);
+
+		} 
+		else if ($('#menu').hasClass("menuFixed") && ($(window).scrollTop() < $('#menu').data("top"))) {
+			$('#menu').removeClass("menuFixed");
+			$('#menu').addClass("menuBack")
+		}
+	});*/
+
+	$(window).scroll(function() {
+		if(!$('#header').hasClass("elemFixed") && ($(window).scrollTop() > $('#header').offset().top)) {
+			$('#header').removeClass("elemeInit");
+			$('#header').addClass("elemFixed").data("top", $('#header').offset().top);
+			$('#menu').removeClass("menuBack");
+			$('#menu').addClass("menuFixed").data("top", $('#menu').offset().top);
+			
+		} 
+		else if ($('#header').hasClass("elemFixed") && ($(window).scrollTop() < $('#header').data("top"))) {
+			$('#header').removeClass("elemFixed");
+			$('#header').addClass("elemeInit")
+			$('#menu').removeClass("menuFixed");
+			$('#menu').addClass("menuBack")
+		}
+	});
+</script>
+
 
 <?php include("fonctionsPHP/fonctionEspace.php");
